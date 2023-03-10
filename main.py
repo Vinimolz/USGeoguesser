25f2b195bd753030fe5b59e010b545af112ceb4d
Vinimolz <76082483+Vinimolz@users.noreply.github.com>
2023-03-10 12:28:15 -0800

    Finishing project

--- main.py
+++ main.py
@@ -1,9 +1,43 @@
 import csv
 import pandas
+import turtle
 
 def main():
-   squirrel_data = get_squirrel_data()
-   print(squirrel_data)
+    screen = turtle.Screen()
+    screen.title = 'U.S GeoGuesser'
+    background_image_path = 'blank_states_img.gif'
+    screen.addshape(background_image_path)
+    turtle.shape(background_image_path) 
+    
+    data = pandas.read_csv("data/50_states.csv") 
+
+    guessed_states = []
+
+    state_data_list = get_states_data_list()    
+    
+    while len(guessed_states) < 50:
+        user_input = screen.textinput(f'U.S Geoguesser {len(guessed_states)}/50',
+                                       'Guess the name of a state').title()
+        
+        if user_input == 'Exit':
+            missing_states = []
+            for state in state_data_list:
+                if state not in guessed_states:
+                    missing_states.append(state)
+            new_data = pandas.DataFrame(missing_states)
+            new_data.to_csv("states_to_learn.csv")
+            break
+
+        if user_input in state_data_list:
+            guessed_states.append(user_input)
+
+            t = turtle.Turtle()
+            t.hideturtle()
+            t.penup()
+            state_data = data[data.state == user_input]
+            t.goto(int(state_data.x), int(state_data.y))
+            t.write(user_input)
+    
 
 def get_all_data():
     weather_data = []
@@ -29,6 +63,11 @@ def get_data_with_pandas():
 
 def get_squirrel_data():
     data = pandas.read_csv("data/squirrel_data.csv")
+    return data
+
+def get_states_data_list():
+    data = pandas.read_csv('data/50_states.csv')
+    data = data.state.to_list()
     return data
 
 if __name__ == '__main__':
